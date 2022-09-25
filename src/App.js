ca55597ff568df4d81aafd093f681429d83ce94b
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-25 15:10:24 -0700

    shared layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Error from './pages/Error';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+    <nav>Our navbar</nav>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path='about' element={<About />} />
+          <Route path='products' elements={<Products />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+      <footer>Our footer</footer>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
+import { Link, Outlet } from "react-router-dom";
+
 const Home = () => {
   return (
     <section className='section'>
       <h2>Home Page</h2>
+      <Outlet />
     </section>
   );
 };
